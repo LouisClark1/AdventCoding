3abed75564ec7bac9d303fbbb62d4f7f0e3149b9
LouisUbuntu <louis.clark2410@gmail.com>
2021-12-17 10:36:10 +0000

    the shrinking works for one loop but the realloc is not working correctly

--- day3Part2.c
+++ day3Part2.c
@@ -12,10 +12,9 @@ int sumAColumn (int *arrayPointer, int colNum, int *arraySizePointer){
     int localBinNum = *arraySizePointer;
     int localFileLength = *(arraySizePointer+1);
     int arraySum = 0;
-
     for(int j=0;j<localFileLength;j++) {
         arraySum += *(arrayPointer + colNum + j * localBinNum);
-        printf("array *arrayPointer %i\n", *(arrayPointer + colNum + j * localBinNum));
+        // printf("array *arrayPointer %i\n", *(arrayPointer + colNum + j * localBinNum));
     }
 
 
@@ -51,7 +50,6 @@ int main(){
         printf("failed to open %s\n", fileName);
         return -1;
     }
-
     int i = 1;
     int fileValue;
     while (fileValue != EOF){
@@ -80,7 +78,6 @@ int main(){
     //     printf("FAIL");
     // }
     // free(pDataArray);
-
     //=================populating the data array with data================
     rewind(pFile);
     int rowNumCount = 0;
@@ -94,10 +91,8 @@ int main(){
         dataArray[rowNumCount][dataCounter%(binNum + 1)] = dataRead;
         dataCounter++;
     }
-
     fclose(pFile);
     pFile = NULL;
-
     //================string array to int array=========================
     int j;
     for(i=0; i < rowNum; i++) {
@@ -107,38 +102,101 @@ int main(){
             dataArray[i][j] = atoi(tempString);
       }
     }
-    //===================check for most commin first value=================
- 
-    int dataArraySize[2] = {binNum, rowNum};
-    int *pDataArraySize = dataArraySize;
-    int arrayColSum = sumAColumn(pDataArray,0, pDataArraySize);//col starts at 0
-    int binChoice = 0;
-    if(arrayColSum > dataArraySize[1]/2){
-            printf("The choice is 1\n");
-            binChoice = 1;
-        }else{
-            printf("The choice is 0\n");
+
+    int L = 0;
+    int newArrayLength = rowNum;
+    int oldArrayLength = newArrayLength;
+
+    //==============make a temp array for the old data=========================
+    printf("This is the complete array\n");
+    int * pTempDataArrayOld = malloc( (newArrayLength * binNum) * sizeof(int) );
+    for(i=0; i < newArrayLength; i++) {
+        for(j=0; j < binNum; j++) {
+        *(pTempDataArrayOld + j + (i * binNum) ) = dataArray[i][j];
+        printf("%i ", *(pTempDataArrayOld + j + (i * binNum) ));
         }
-    //=======================count the size of the next array to make===========
-    int newArrayLength;
-    for(j=0;j<rowNum;j++) {
-        if (dataArray[j][0] == binChoice){
-            printf("data from loop %i & bin choice %i",dataArray[j][0], binChoice);
-            // newArrayLength++;                
-        }
+        printf("\n");
     }
-    // printf("New array length is %i", newArrayLength);
-    
-//==================================making new reduced arrays===============
-    int tempRowNum, tempBinNum;
-
-    // int dataArray[rowNum][binNum];
-    // int * pDataArray = NULL;
+    printf("=============================\n");
 
 
 
+    while (newArrayLength != 1)
+    {
+                
+        //===================check for most commin first value=================
+        int dataArraySize[2] = {binNum, rowNum};
+        int *pDataArraySize = dataArraySize;
+        int arrayColSum = sumAColumn(pTempDataArrayOld,0, pDataArraySize);//col starts at 0
+        int binChoice = 0;
+        if(arrayColSum < dataArraySize[1]/2 - 1){
+                printf("The choice is 0 for column %i\n", L);
+            }else{
+                printf("The choice is 1 for column %i\n", L);
+                binChoice = 1;
+            }
 
-    printf("\n\n\n It ran ok");
+        //=======================count the size of the next array to make===========
+        L = 0;
+        newArrayLength = 0;
+        for(i=0; i < 10; i++) {
+            for(j=0; j < binNum; j++) {
+                printf("%i ",*(pTempDataArrayOld + j + (i * binNum)));
+            }
+            printf("\n");
+        }
+        printf("matching the bin to choice\n");
+        for(j=0;j<oldArrayLength+1;j++) {
+            if (*(pTempDataArrayOld + L + j * binNum) == binChoice){
+                newArrayLength++;                
+            }
+            printf("%i",*(pTempDataArrayOld + L + j * binNum));
+        }
+        printf("\n");
+        //==================================making new reduced array from old===============
+
+        // int tempdataArray[newArrayLength][binNum];
+        int * pTempDataArray = malloc( (newArrayLength * binNum) * sizeof(int) );
+        // pTempDataArray = tempdataArray[0];
+        printf("This is the new reduced size array \" temp data array \" with size %i\n",newArrayLength);
+        i = 0;
+        int k = 0;
+        for(i=0; i < oldArrayLength; i++) {
+        if (*(pTempDataArrayOld + L + (i * binNum)) == binChoice){
+            
+            // printf("This is the logic compared to bin choice %i\n", *(pTempDataArrayOld + L + (i * binNum)));
+                for(j=0; j < binNum; j++) {
+                    *(pTempDataArray + j + (k * binNum) ) = *(pTempDataArrayOld + j + (i * binNum) );
+                    printf("%i ", *(pTempDataArray + j + (k * binNum) ));
+                }
+                printf("\n");
+                k++;
+            }
+        }
+        L++;
+        //=====================make current into old====================================
+        // printf("This is making the current into the old \" temp data array old \"\n");
+        int * pTempDataArrayOld = realloc(pTempDataArrayOld, (newArrayLength * binNum) * sizeof(int) );
+        for(i=0; i < newArrayLength; i++) {
+            for(j=0; j < binNum; j++) {
+                *(pTempDataArrayOld + j + (i * binNum) ) = *(pTempDataArray + j + (i * binNum) );
+                // printf("%i ", *(pTempDataArrayOld + j + (i * binNum) ));
+            }
+            // printf("\n");
+        }
+        oldArrayLength = newArrayLength;
+
+        //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! we may not need old array length anywhere
+        free(pTempDataArray);
+        pTempDataArray = NULL;
+
+        printf("=============================\n");
+    }
+
+    free(pTempDataArrayOld);
+    pTempDataArrayOld = NULL;
+
+    printf("\n\nIt ran ok\n");
 
     return 0;
 }
